74304f0733b2d7ed2de07a0e7de8f8bd8357e000
Etpaxtysus <itsrhenaldy@gmail.com>
2025-01-30 15:56:46 +0700

    update: update main page

--- lib/pages/app/main_page.dart
+++ lib/pages/app/main_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/pages/app/get_started_page.dart';
 import 'package:myapp/pages/app/news_page.dart'; // Mengimpor NewsPage yang baru
